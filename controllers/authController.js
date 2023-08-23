fa75d414a42ebe872b5841821c73f3f601dbd212
giacomo accursi <giacomo.accursi@gmail.com>
2023-08-23 16:30:42 +0200

    feat: add instruction registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
-const Athlete = require("../models/Athlete");
 const bcrypt = require("bcrypt");
+const Athlete = require("../models/Athlete");
+const Instructor = require("../models/Instructor");
 
 async function registerAthlete(req, res) {
   const {
@@ -55,4 +56,52 @@ async function registerAthlete(req, res) {
   }
 }
 
-module.exports = { registerAthlete };
+async function registerInstructor(req, res) {
+  const {
+    email,
+    name,
+    surname,
+    birthdate,
+    gender,
+    password,
+    password_confirm,
+  } = req.body;
+
+  if (
+    !email ||
+    !name ||
+    !surname ||
+    !birthdate ||
+    !gender ||
+    !password ||
+    !password_confirm
+  ) {
+    return res.status(422).json({ message: "Invalid fields" });
+  }
+
+  if (password !== password_confirm)
+    return res.status(422).json({ message: "Passwords do not match" });
+
+  const userExists = await Instructor.exists({ email }).exec();
+
+   if (userExists) return res.sendStatus(409);
+
+  try {
+    hashedPassword = await bcrypt.hash(password, 10);
+
+    await Instructor.create({
+      email,
+      name,
+      surname,
+      birthdate,
+      gender,
+      password: hashedPassword,
+    });
+
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.status(400).json({ message: "Could not register" });
+  }
+}
+
+module.exports = { registerAthlete, registerInstructor };
