51a164b8bdadb5bbb4fd3a0022045438761e4cfb
giacomo accursi <giacomo.accursi@gmail.com>
2023-08-23 17:01:02 +0200

    feat: add user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,23 @@ const Athlete = require("../models/Athlete");
 const Instructor = require("../models/Instructor");
 
 async function registerAthlete(req, res) {
+  await registerUser(req, res, Athlete);
+}
+
+async function registerInstructor(req, res) {
+  await registerUser(req, res, Instructor);
+}
+
+
+async function loginAthlete(req, res) {
+  await authenticateUser(req, res, Athlete);
+}
+
+async function loginInstructor(req, res) {
+  await authenticateUser(req, res, Instructor);
+}
+
+async function registerUser(req, res, User) {
   const {
     email,
     name,
@@ -19,8 +36,8 @@ async function registerAthlete(req, res) {
     !email ||
     !name ||
     !surname ||
-    !weight ||
-    !height ||
+    (!height && User === Athlete) ||
+    (!weight && User === Athlete) ||
     !birthdate ||
     !gender ||
     !password ||
@@ -29,17 +46,20 @@ async function registerAthlete(req, res) {
     return res.status(422).json({ message: "Invalid fields" });
   }
 
-  if (password !== password_confirm)
+  if (password !== password_confirm) {
     return res.status(422).json({ message: "Passwords do not match" });
+  }
 
-  const userExists = await Athlete.exists({ email }).exec();
+  const userExists = await User.exists({ email }).exec();
 
-   if (userExists) return res.sendStatus(409);
+  if (userExists) {
+    return res.sendStatus(409);
+  }
 
   try {
-    hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    await Athlete.create({
+    await User.create({
       email,
       name,
       surname,
@@ -56,52 +76,26 @@ async function registerAthlete(req, res) {
   }
 }
 
-async function registerInstructor(req, res) {
-  const {
-    email,
-    name,
-    surname,
-    birthdate,
-    gender,
-    password,
-    password_confirm,
-  } = req.body;
+async function authenticateUser(req, res, User) {
+  const { email, password } = req.body;
 
-  if (
-    !email ||
-    !name ||
-    !surname ||
-    !birthdate ||
-    !gender ||
-    !password ||
-    !password_confirm
-  ) {
-    return res.status(422).json({ message: "Invalid fields" });
+  if (!email || !password) {
+    return res.status(422).json({ message: 'Invalid fields' });
   }
 
-  if (password !== password_confirm)
-    return res.status(422).json({ message: "Passwords do not match" });
+  const user = await User.findOne({ email }).exec();
 
-  const userExists = await Instructor.exists({ email }).exec();
-
-   if (userExists) return res.sendStatus(409);
-
-  try {
-    hashedPassword = await bcrypt.hash(password, 10);
-
-    await Instructor.create({
-      email,
-      name,
-      surname,
-      birthdate,
-      gender,
-      password: hashedPassword,
-    });
-
-    return res.sendStatus(201);
-  } catch (error) {
-    return res.status(400).json({ message: "Could not register" });
+  if (!user) {
+    return res.status(401).json({ message: 'Email or password is incorrect' });
   }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    return res.status(401).json({ message: 'Email or password is incorrect' });
+  }
+
+  return res.sendStatus(200);
 }
 
-module.exports = { registerAthlete, registerInstructor };
+module.exports = { registerAthlete, registerInstructor, loginAthlete, loginInstructor };
