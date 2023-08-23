2b4be8a30cfbccbc8e69fe53f1390e9718591792
giacomo accursi <giacomo.accursi@gmail.com>
2023-08-23 11:50:28 +0200

    chore: delete example routes

--- app.js
+++ app.js
@@ -3,16 +3,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
-
-// Define a route for /about
-app.get('/about', (req, res) => {
-  res.send('About page');
-});
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
