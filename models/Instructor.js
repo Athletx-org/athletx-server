969803585064c8c09cf1a6af6169e705ee6ca576
giacomo accursi <giacomo.accursi@gmail.com>
2023-08-23 16:29:14 +0200

    chore: add instructor model

---
+++ models/Instructor.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const InstructorSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      validate: [
+        (val) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val),
+      ],
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    surname: {
+      type: String,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+    },
+
+    gender: {
+      type: String, 
+      required: true
+    },
+
+    birthdate: {
+      type: Date, 
+      required: true
+    },
+
+    refresh_token: String,
+  }
+);
+
+module.exports = mongoose.model("Instructor", InstructorSchema);
