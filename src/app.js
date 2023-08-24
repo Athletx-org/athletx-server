a53f5fb89ca01345b81e63a2703e0d1487d4b343
giacomo accursi <giacomo.accursi@gmail.com>
2023-08-24 17:34:57 +0200

    chore: add cookie-parser and cors middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const connectDB = require('./config/database')
+const cors = require('cors')
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 const port = 3000;
@@ -9,6 +12,7 @@ connectDB()
 
 app.use(express.json())
 app.use(cors());
+app.use(cookieParser());
 app.use('/api/auth', require('./routes/api/auth'))
 
 mongoose.connection.once('open', ()=>{
