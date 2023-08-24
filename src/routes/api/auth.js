1301f7904817f122818103a746327339c7c5e556
giacomo accursi <giacomo.accursi@gmail.com>
2023-08-24 17:40:14 +0200

    style: reformat code

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const authControllers = require('../../controllers/authController')
+const express = require("express");
+const router = express.Router();
+const authControllers = require("../../controllers/authController");
 
-
-router.post('/registerAthlete', authControllers.registerAthlete)
-router.post('/registerInstructor', authControllers.registerInstructor)
-router.post('/loginInstructor', authControllers.loginInstructor)
-router.post('/loginAthlete', authControllers.loginAthlete)
-
-module.exports = router
+router.post("/registerAthlete", authControllers.registerAthlete);
+router.post("/registerInstructor", authControllers.registerInstructor);
+router.post("/loginInstructor", authControllers.loginInstructor);
+router.post("/loginAthlete", authControllers.loginAthlete);
+module.exports = router;
